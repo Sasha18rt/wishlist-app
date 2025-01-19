19159bea447613d8ff9cf8d602b083d2485d0e77
Rotaienko Oleksandr <sasharotaenko1@gmail.com>
2025-01-20 01:48:36 +0200

    ssl: false

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            ssl: false, 
+          });
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
